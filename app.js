cdec293397e10e4c75b083e38287d3d0c655db78
Tommi <tmmi.makela@outlook.com>
2022-02-10 18:53:27 +0200

    feat: Add mongodb connection

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+const config = require('./utils/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+  });
 
 app.get('/ping', (req, res) => {
   res.end('pong');
