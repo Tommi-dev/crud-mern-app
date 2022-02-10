b1079cfe55adf767bb0a314c14e24c2463fa3cfa
Tommi <tmmi.makela@outlook.com>
2022-02-10 18:05:39 +0200

    chore: Create http server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello world');
+const http = require('http');
+const config = require('./utils/config');
+
+const server = http.createServer();
+
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
+});
