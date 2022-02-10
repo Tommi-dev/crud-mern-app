c80303bced36c695e42e70b3988582bd97cc4be6
Tommi <tmmi.makela@outlook.com>
2022-02-10 18:08:22 +0200

    docs: Add comments

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+/**
+ * Module dependencies
+ */
 const http = require('http');
 const config = require('./utils/config');
 
+/**
+ * Create http server
+ */
 const server = http.createServer();
 
+// Listener to the specified port
 server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
 });
