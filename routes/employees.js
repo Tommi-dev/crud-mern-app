106329c9d8b0ab1c0fba24d485e6759e680ebd1b
Tommi <tmmi.makela@outlook.com>
2022-02-11 10:29:13 +0200

    feat: Add a feature to retrieve all employees from the database

--- routes/employees.js
+++ routes/employees.js
@@ -2,13 +2,23 @@
  * Module dependencies
  */
 const employeesRouter = require('express').Router();
+const EmployeeModel = require('../models/employee');
 
 /**
  * Get all employees
  */
 employeesRouter.get('/', async (req, res) => {
 
-  res.status(200).end();
+  try {
+
+    const employees = await EmployeeModel.find();
+    res.send(employees);
+
+  } catch(err) {
+
+    return res.status(500).json({ message: err.message });
+
+  }
 
 });
 
