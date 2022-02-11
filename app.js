80163e54f22b7378cf0f61732687771a7637ffdf
Tommi <tmmi.makela@outlook.com>
2022-02-11 08:14:49 +0200

    docs: Add comments

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+/**
+ * Module dependencies
+ */
 const config = require('./utils/config');
 const express = require('express');
 const mongoose = require('mongoose');
+
+// Express app
 const app = express();
 
+/**
+ * MongoDB connection
+ */
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
@@ -11,6 +19,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
     console.error(`Error connecting to MongoDB: ${error.message}`);
   });
 
+// Route for testing
 app.get('/ping', (req, res) => {
   res.end('pong');
 });
