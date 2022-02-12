9a0b1ffbab2f7afe5ce7759eed7694978b36baab
Tommi <tmmi.makela@outlook.com>
2022-02-12 18:28:40 +0200

    chore: Add CORS

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const employeesRouter = require('./routes/employees');
 const morgan = require('morgan');
 const { errorHandler, unknownEndpoint } = require('./utils/middleware');
+const cors = require('cors');
 
 // Express app
 const app = express();
@@ -26,6 +27,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
  * Middlewares for handling request/response objects
  */
 app.use(express.json());
+app.use(cors());
 
 if (process.env.NODE_ENV !== 'test') {
   app.use(morgan('tiny'));
